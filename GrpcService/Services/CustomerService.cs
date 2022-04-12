7d6fa3f0c54e2fe0baed5d0c3ab9625581aaf9ea
Hakan GÜZEL <hakan-guzel@outlook.com>
2022-04-12 10:38:17 +0300

    Initial commit

---
+++ GrpcService/Services/CustomerService.cs
@@ -0,0 +1,55 @@
+﻿using Grpc.Core;
+
+namespace GrpcService.Services
+{
+    public class CustomerService : Customer.CustomerBase
+    {
+        private readonly ILogger<CustomerService> _logger;
+        public CustomerService(ILogger<CustomerService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<CustomersDataModel> GetCustomersInfo(EmptyModel request, ServerCallContext context)
+        {
+            CustomersDataModel result = new CustomersDataModel();
+            result.Items.Add(new CustomerDataModel { FirstName = "FirstName1", LastName = "LastName1" });
+            result.Items.Add(new CustomerDataModel { FirstName = "FirstName2", LastName = "LastName2" });
+            result.Items.Add(new CustomerDataModel { FirstName = "FirstName3", LastName = "LastName3" });
+            result.Items.Add(new CustomerDataModel { FirstName = "FirstName4", LastName = "LastName4" });
+            result.Items.Add(new CustomerDataModel { FirstName = "FirstName5", LastName = "LastName5" });
+            result.Items.Add(new CustomerDataModel { FirstName = "FirstName6", LastName = "LastName6" });
+            return Task.FromResult(result);
+        }
+        public override Task<CustomerDataModel> GetCustomerInfo(CustomerFindModel request, ServerCallContext context)
+        {
+            CustomerDataModel result = new CustomerDataModel();
+
+            // This is a sample code for demo
+            // in real life scenarios this information should be fetched from the database
+            // no data should be hardcoded in the application
+            if (request.UserId == 1)
+            {
+                result.FirstName = "Mohamad";
+                result.LastName = "Lawand";
+            }
+            else if (request.UserId == 2)
+            {
+                result.FirstName = "Richard";
+                result.LastName = "Feynman";
+            }
+            else if (request.UserId == 3)
+            {
+                result.FirstName = "Bruce";
+                result.LastName = "Wayne";
+            }
+            else
+            {
+                result.FirstName = "James";
+                result.LastName = "Bond";
+            }
+
+            return Task.FromResult(result);
+        }
+    }
+}
---
+++ gRPCClient/Program.cs
@@ -0,0 +1,30 @@
+﻿// We create a channel that represents the connection from client to the server
+// The URL that we add is provided from Kestrel in the server
+using Grpc.Net.Client;
+using GrpcService;
+
+var channel = GrpcChannel.ForAddress("http://localhost:5115");
+
+// this the strongly typed client that was create for us from code generation
+// when we added the .proto file
+
+var customerClient = new Customer.CustomerClient(channel);
+
+var result = await customerClient.GetCustomerInfoAsync(new CustomerFindModel()
+{
+    UserId = 1
+});
+
+Console.WriteLine($"First Name: {result.FirstName} - Last Name: {result.LastName}");
+
+var result2 = await customerClient.GetCustomersInfoAsync(new EmptyModel { });
+
+if(result2 != null)
+    foreach (var item in result2.Items)
+    {
+        Console.WriteLine($"First Name: {item.FirstName} - Last Name: {item.LastName}");
+    }
+
+
+
+Console.WriteLine($"First Name: {result.FirstName} - Last Name: {result.LastName}");
